a191f25d40fec9afb9be78560477f57c17952fca
Pablo Jurado <pjurado85@gmail.com>
2017-03-22 07:45:09 -0500

    update exercises.js

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # week_6_lecture_2_exercises
+I am a front end engineering student at the Iron Yard. This is our homework about JavaScript global objects.
---
+++ exercises.js
@@ -0,0 +1,37 @@
+// Complete both of the following tasks:
+
+// Review the list of global built-in objects on MDN - Global Objects. Disregarding
+// global properties and functions, list three global objects you recall learning
+// about in class and write a one sentence description of that object's purpose.
+
+// 'Math: This object allows you to perform mathematical operations,
+// like rounding numbers, generating random numbers, maximum, minimum, etc.'
+
+// 'Date: This object lets us work with dates. You can manipulate data like
+//  years, months, days, hours, minutes, seconds, and milliseconds.
+
+// 'JSON: This object lets you convert any JavaScript object into JSON notation,
+// so we can exchange data between a browser and a server
+// We can also convert any JSON received from the server into JavaScript objects.'
+
+// Review the list of global built-in objects on MDN - Global Objects.
+// Disregarding global properties and functions, list three global objects you
+// have not learned about in class. Review the documentation for that object
+// and write a one sentence description of its purpose.
+
+// Set: The Set object lets you store unique values of any type, whether primitive values or object references.
+// WeakMap: This object is a collection of key/value pairs in which the keys are weakly referenced.  The keys must be objects and the values can be arbitrary values.
+// Promise: This object is used for asynchronous computations. A Promise represents a value which may be available now, or in the future, or never.
+
+// Write a program that does the following:
+// Generate a random decimal number between 0 and 100 (EG: 93.60745257237626) and store it in a variable.
+
+// Round the number to the nearest integer (EG: 94)
+
+// Round the number up to the next largest integer (EG: 94)
+
+// Round the number down to the next smallest integer (EG: 93)
+
+// Print all four numbers out with labels.
+
+// Write JavaScript code that generates a random integer in the range -10 to 10.
